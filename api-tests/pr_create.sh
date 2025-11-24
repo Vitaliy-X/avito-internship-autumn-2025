bc61c6bf256d337fa628d953b3e8fc7d11d32c40
Vitaliy-X <vitaliy_x_1@mail.ru>
2025-11-24 10:46:57 +0300

    fix: update db method

--- api-tests/pr_create.sh
+++ api-tests/pr_create.sh
@@ -3,7 +3,7 @@
 curl -X POST http://localhost:8080/pullRequest/create \
   -H "Content-Type: application/json" \
   -d '{
-    "pull_request_id": "pr-1001",
-    "pull_request_name": "Add search",
+    "pull_request_id": "pr-1",
+    "pull_request_name": "Add something",
     "author_id": "u1"
   }'
